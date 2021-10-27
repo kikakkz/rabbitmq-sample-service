73940afa495335cb081b9ce3680d91befda59fbc
Zhao KK <kikakkz@hotmail.com>
2021-10-27 15:07:08 +0000

    Support rabbitmq client

--- cmd/rabbitmq-sample/run.go
+++ cmd/rabbitmq-sample/run.go
@@ -6,6 +6,8 @@ import (
 	cli "github.com/urfave/cli/v2"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	msgcli "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/client"
+	"github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/common" //nolint
 	msgsrv "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/server"
 )
 
@@ -19,11 +21,32 @@ var runCmd = &cli.Command{
 	},
 }
 
+func consumer(id int) {
+	_cli, err := msgcli.New(rabbitmq.MyServiceNameToVHost())
+	if err != nil {
+		logger.Sugar().Errorf("fail to create rabbitmq client: %v", err)
+		return
+	}
+
+	msgs, err := _cli.Consume("common")
+	if err != nil {
+		logger.Sugar().Errorf("fail to consume msgs: %v", err)
+		return
+	}
+
+	for d := range msgs {
+		logger.Sugar().Infof("consume '%s' by %v", d.Body, id)
+	}
+}
+
 func runRabbitMQSample() error {
 	err := msgsrv.DeclareQueue("common")
 	if err != nil {
 		return err
 	}
+
+	go consumer(1)
+	go consumer(2)
 
 	for {
 		logger.Sugar().Infof("Publish hello world")
